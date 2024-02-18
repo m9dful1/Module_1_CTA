2af746828cd785056be44ad5833e00c6132dae7c
m9dful1 <160385370+m9dful1@users.noreply.github.com>
2024-02-18 00:44:00 -0700

    Initial commit

---
+++ src/PersonalInformation.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PersonalInformation {
+    public static void main(String[] args) {
+        // Variables to store the fictional person's information
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "876 Washington St.";
+        String city = "Denver";
+        String zipCode = "80229";
+
+        // Print each piece of information on a separate line
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
